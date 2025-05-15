ee7960edf43f0d190e1c74b277fa71a1ec73f900
apoorv <apoorv654123@gmail.com>
2025-05-15 15:00:43 +0530

    socket complete

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+import { app, server } from "./lib/socket.js";
+
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import messageRoutes from "./routes/message.route.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+
+server.listen(PORT, () => {
+  console.log("Server is running on PORT:" + PORT);
+  connectDB();
+});
